afddf65b7644000ad1d1934449c1f4c409ac16df
Pavithra-Sree <nirmalsree16@gmail.com>
2023-06-19 22:57:43 +0530

    new-project-pavi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import {Route, Routes} from "react-router-dom";
 import './App.css';
+import axios from "axios";
+import Nav from "./Components/Nav";
+import Feed from "./Components/Video/Feed";
+import Player from "./Components/Video/Player";
+import Create from "./Components/Video/Create";
+import { useEffect, useState } from "react";
+import NewData from "./Components/Music/NewData";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Edit from "./Components/Video/Edit";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await axios.get(
+          "https://648bfc4a8620b8bae7ec01dc.mockapi.io/users"
+        );
+        setData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Feed data={data} />} />
+        <Route path="/play/:id" element={<Player />} />
+        <Route
+          path="/create"
+          element={<Create data={data} setData={setData} />}
+        />
+
+        <Route path="/music" element={<NewData />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/edit/:id" element={<Edit />} />
+      </Routes>
     </div>
   );
 }
